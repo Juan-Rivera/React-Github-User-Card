e66286831a2d228384e64fe4d89ee0efe8d547b3
Juan (programming) <programjuan@Juans-MacBook-Pro.local>
2020-05-06 17:44:25 -0400

    Set up App.js and Card.js with simple class components that just render on the screen

---
+++ github-card/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './App.css';
+
+class App extends React.Component{
+
+  render(){
+    return(
+      <div className='App'>
+      This is on the screen :)
+    
+      </div>
+    )
+  }
+}
+
+export default App;
---
+++ github-card/src/components/Card.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Card extends Component{
+    render(){
+        return (
+            <div className='cardC'>
+                This is also on the screen :D
+            </div>
+        )
+    }
+}
+export default Card
